ddea7463c5239e3cf1df473190ae941f5ea73910
Sheikh Qumruzzaman <alamin141@gmail.com>
2019-07-10 23:28:24 -0700

    routing url shortening service through zuul

---
+++ zuul-api-gateway/src/main/java/com/sheikh/umayer/zuulapigateway/ZuulLoggingFilter.java
@@ -0,0 +1,39 @@
+package com.sheikh.umayer.zuulapigateway;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		return false;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+		logger.info("request -> {} request uri -> {}", req, req.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
