0d88ff7064067870860fcf03a7b1c4184360ca60
Sheikh Qumruzzaman <alamin141@gmail.com>
2019-07-27 21:58:02 -0700

    CSRF works without CORS as well

--- login/server.js
+++ login/server.js
@@ -26,7 +26,7 @@ var users = [{"username" : "sheikh"}];
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
